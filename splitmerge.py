58f922802b856c02448f779aedea93c46e8a8874
fadetempest <fadetempest@icloud.com>
2024-02-29 02:12:35 +0400

    New changes

--- splitmerge.py
+++ splitmerge.py
@@ -16,6 +16,8 @@ img_res = resize_img(img, scale=0.25)
 
 cv.imshow("rengar", img_res)
 
+blank = np.zeros(img_res.shape[:2], dtype='uint8')
+
 # Split color channels
 
 b,g,r = cv.split(img_res)
@@ -23,5 +25,8 @@ b,g,r = cv.split(img_res)
 cv.imshow("Blue", b)
 cv.imshow("Green", g)
 cv.imshow("Red", r)
+
+green = cv.merge([blank, g, blank])
+cv.imshow('Gr', green)
 
 cv.waitKey(0)
